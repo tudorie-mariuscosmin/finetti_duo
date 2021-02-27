6112b8ce935aa043557768c64ecd695ab5737fbf
Marius Tudorie <tudorie.mariuscosmin@gmail.com>
2021-02-27 16:07:39 +0200

    store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 // import example from './module-example'
+import fireStore from './fire'
 
 Vue.use(Vuex)
 
@@ -17,7 +18,7 @@ Vue.use(Vuex)
 export default function (/* { ssrContext } */) {
   const Store = new Vuex.Store({
     modules: {
-      // example
+      fireStore
     },
 
     // enable strict mode (adds overhead!)
