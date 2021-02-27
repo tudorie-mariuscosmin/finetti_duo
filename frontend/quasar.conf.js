2d0dce7d6f26dbf3b96730f0c0cbef840b193d92
Marius Tudorie <tudorie.mariuscosmin@gmail.com>
2021-02-27 16:25:01 +0200

    started auth

--- frontend/quasar.conf.js
+++ frontend/quasar.conf.js
@@ -86,6 +86,7 @@ module.exports = function (/* ctx */) {
       lang: 'en-us', // Quasar language pack
       config: {},
 
+
       // Possible values for "importStrategy":
       // * 'auto' - (DEFAULT) Auto-import needed Quasar components & directives
       // * 'all'  - Manually specify what to import
@@ -99,7 +100,7 @@ module.exports = function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [" LocalStorage"]
     },
 
     // animations: 'all', // --- includes all animations
--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
 import axios from 'axios'
+import LocalStorage from 'quasar'
 
 Vue.prototype.$axios = axios
+
+const token = LocalStorage.getItem("token")
+if (token) {
+    Vue.prototype.$axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+}
